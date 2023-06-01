4ac66b4482d5df42c3e82563edf474179856b010
saitejasaluri12 <saiteja.saluri12@gmail.com>
2023-06-01 15:28:49 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args)
+        {
+        student1 s=new student1();
+        s.setName("SaiTeja");
+        System.out.println("Name : " +s.getName());
+
+        }
+}
---
+++ src/student1.java
@@ -0,0 +1,13 @@
+public class student1 {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+
+    }
+}
+
